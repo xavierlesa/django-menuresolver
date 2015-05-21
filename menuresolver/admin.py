69581ce7815d5af3b6f7efc4ee075e24be74670a
Xavier <xavier@link-b.com>
2015-05-21 16:38:14 -0300

    add admin

--- menuresolver/admin.py
+++ menuresolver/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
-# Register your models here.
+# -*- coding:utf-8 -*-
 
+from django.contrib import admin
+from menuresolver.models import Menu, Item
+
+
+class ItemInline(admin.TabularInline):
+    model = Item
+
+class MenuAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemInline        
+    ]
+
+admin.site.register(Menu, MenuAdmin)
